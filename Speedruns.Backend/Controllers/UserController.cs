4c84cbf7c84c4fb7d19b1848c5d999235b5aa498
Splix1 <unversed1241@yahoo.com>
2023-10-14 16:37:20 -0700

    (FEAT): controller route to get all users

--- Speedruns.Backend/Controllers/UserController.cs
+++ Speedruns.Backend/Controllers/UserController.cs
@@ -1,10 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Speedruns.Backend.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Speedruns.Backend.Controllers
 {
+    [Route("api/users")]
+    [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DbSet<UserModel> _users;
+
+        public UserController(SpeedrunsContext context)
+        {
+            _users = context.Users;
+        }
+
+        // GET: /api/users
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> GetAll()
+        {
+            try
+            {
+                var users = _users.ToListAsync();
+                return Ok(users);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}\nStack Trace: {ex.StackTrace}");
+                return StatusCode(500);
+            }
+        }
     }
 }
