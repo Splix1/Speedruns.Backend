e3ef3ab1ea1752d878072d5563f9019de4c1454c
Splix1 <unversed1241@yahoo.com>
2023-10-15 09:56:44 -0700

    (FEAT): run interface initialized

---
+++ Speedruns.Backend/Interfaces/IRunRepository.cs
@@ -0,0 +1,13 @@
+﻿using Speedruns.Backend.Models;
+
+namespace Speedruns.Backend.Interfaces
+{
+    public interface IRunRepository
+    {
+        Task<List<RunModel>> GetAll();
+        Task<RunModel> GetById(long id);
+        Task<RunModel> CreateRun(RunModel run);
+        Task<RunModel> UpdateRun(long id, RunModel run);
+        Task<RunModel> DeleteRun(long id);
+    }
+}
