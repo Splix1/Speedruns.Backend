b830698af98b72e32548e0a8ebdde80a44386a4b
Splix1 <unversed1241@yahoo.com>
2023-10-15 14:49:20 -0700

    (FEAT): comments repository created

---
+++ Speedruns.Backend/Interfaces/ICommentsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Speedruns.Backend.Models;
+
+namespace Speedruns.Backend.Interfaces
+{
+    public interface ICommentsRepository
+    {
+        Task<List<CommentModel>> GetComments(long runId);
+    }
+}
---
+++ Speedruns.Backend/Repositories/CommentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Speedruns.Backend.Interfaces;
+using Speedruns.Backend.Models;
+
+namespace Speedruns.Backend.Repositories
+{
+    public class CommentsRepository : ICommentsRepository
+    {
+        private readonly SpeedrunsContext _context;
+        private readonly DbSet<CommentModel> _comments;
+
+        public CommentsRepository(SpeedrunsContext context)
+        {
+            _context = context;
+            _comments = context.Comments;
+        }
+        public async Task<List<CommentModel>> GetComments(long runId)
+        {
+            return await _comments.Where(comment => comment.RunId == runId).ToListAsync();
+        }
+    }
+}
