c6d00f44057d3496a57a7c7fb5eeaf352a64b261
Splix1 <unversed1241@yahoo.com>
2023-10-16 17:03:58 -0700

    (FEAT): fetching runs includes their game, series and console

--- Speedruns.Backend/Repositories/GamesRepository.cs
+++ Speedruns.Backend/Repositories/GamesRepository.cs
@@ -15,17 +15,17 @@ namespace Speedruns.Backend.Repositories
 
         public async Task<List<GameModel>> GetAll()
         {
-            return await _games.Include(x => x.GameConsoles).ThenInclude(x => x.Console).ToListAsync();
+            return await _games.Include(x => x.Series).Include(x => x.GameConsoles).ThenInclude(x => x.Console).ToListAsync();
         }
 
         public async Task<GameModel> GetById(long id)
         {
-            return await _games.Where(x => x.Id == id).Include(x => x.GameConsoles).ThenInclude(x => x.Console).FirstOrDefaultAsync();
+            return await _games.Where(x => x.Id == id).Include(x => x.Series).Include(x => x.GameConsoles).ThenInclude(x => x.Console).FirstOrDefaultAsync();
         }
 
         public async Task<GameModel> GetByName(string name)
         {
-            return await _games.Include(x => x.GameConsoles).ThenInclude(x => x.Console).FirstOrDefaultAsync(x => x.Name == name);
+            return await _games.Include(x => x.Series).Include(x => x.GameConsoles).ThenInclude(x => x.Console).FirstOrDefaultAsync(x => x.Name == name);
         }
 
     }
