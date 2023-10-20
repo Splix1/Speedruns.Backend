d70bf4c3eb7a24d62210ff3b088d562669855872
Splix1 <unversed1241@yahoo.com>
2023-10-20 10:31:30 -0700

    (FEAT): test for console controller to return a 200

---
+++ Speedruns.Backend.Tests/ConsoleControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Speedruns.Backend.Controllers;
+using NSubstitute;
+using Speedruns.Backend.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Speedruns.Backend.Tests
+{
+    public class ConsoleControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturn200()
+        {
+            // create mock of repository
+            var repositoryMock = Substitute.For<IConsolesRepository>();
+
+            // create new instance of controller using mock repository
+            var controller = new ConsoleController(repositoryMock);
+
+            // make request using controller
+            var response = await controller.GetAll();
+
+            // store result in a variable to test
+            var result = response.Result as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
