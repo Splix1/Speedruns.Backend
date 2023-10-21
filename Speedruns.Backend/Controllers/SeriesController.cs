de873f16358fb6b31b7b06fb1222f53367ac8ba9
Splix1 <unversed1241@yahoo.com>
2023-10-21 12:01:04 -0700

    (FIX): added http binding to series controller

--- Speedruns.Backend/Controllers/SeriesController.cs
+++ Speedruns.Backend/Controllers/SeriesController.cs
@@ -15,6 +15,7 @@ namespace Speedruns.Backend.Controllers
             _series = series;
         }
 
+        [HttpGet]
         public async Task<ActionResult<List<SeriesEntity>>> GetAll()
         {
             try
@@ -32,6 +33,7 @@ namespace Speedruns.Backend.Controllers
             }
         }
 
+        [HttpGet("{id}")]
         public async Task<ActionResult<SeriesEntity>> GetById(long id)
         {
             try
@@ -52,6 +54,7 @@ namespace Speedruns.Backend.Controllers
         }
 
 
+        [HttpGet("{name}")]
         public async Task<ActionResult<SeriesEntity>> GetByName(string name)
         {
             try
