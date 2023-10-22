d2c6d8bd1c75ef15c9f94ac64e7e04adae413d71
Splix1 <unversed1241@yahoo.com>
2023-10-22 12:27:22 -0700

    (FEAT): GetById tests for users controller

--- Speedruns.Backend.Tests/Controllers/UserControllerTests.cs
+++ Speedruns.Backend.Tests/Controllers/UserControllerTests.cs
@@ -13,7 +13,7 @@ namespace Speedruns.Backend.Tests.Controllers
     public class UserControllerTests
     {
         [Fact]
-        public async Task ShouldReturnGetAll()
+        public async Task ShouldReturn200GetAll()
         {
             var repositoryMock = Substitute.For<IUserRepository>();
 
@@ -43,6 +43,43 @@ namespace Speedruns.Backend.Tests.Controllers
             var controller = new UserController(repositoryMockWithError);
 
             var response = await controller.GetAll();
+
+            var result = response.Result as StatusCodeResult;
+
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task ShouldReturn200GetById()
+        {
+            var repositoryMock = Substitute.For<IUserRepository>();
+
+            repositoryMock.GetById(1).Returns(new UserEntity() {
+                Id = 1,
+                UserName = "Test",
+            });
+
+            var controller = new UserController(repositoryMock);
+
+            var response = await controller.GetById(1);
+
+            var result = response.Result as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task ShouldReturn500GetById()
+        {
+            var repositoryMockWithError = Substitute.For<IUserRepository>();
+
+            repositoryMockWithError.GetById(1).ThrowsAsync(new Exception("Internal error"));
+
+            var controller = new UserController(repositoryMockWithError);
+
+            var response = await controller.GetById(1);
 
             var result = response.Result as StatusCodeResult;
 
