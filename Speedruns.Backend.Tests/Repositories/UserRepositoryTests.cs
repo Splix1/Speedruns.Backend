e26bb00e3d3ab12cad8cfcac1a2ea23b8142acfb
Splix1 <unversed1241@yahoo.com>
2023-10-24 14:43:50 -0700

    (FEAT): UserRepository Tests w/ ShouldReturnListOfUsers test

---
+++ Speedruns.Backend.Tests/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using Speedruns.Backend.Entities;
+using Speedruns.Backend.Repositories;
+using Speedruns.Backend.Tests.Database;
+
+namespace Speedruns.Backend.Tests.Repositories
+{
+    public class UserRepositoryTests
+    {
+        private DbContextMock<UserEntity> _dbContextMock;
+        private UserRepository _userRepository;
+
+        public UserRepositoryTests()
+        {
+            _dbContextMock = new DbContextMock<UserEntity>(new List<UserEntity>
+            {
+                new UserEntity { Id = 1, UserName = "Test 1", Runs = new List<RunEntity>{ new RunEntity { Id = 1, UserId = 1, ConsoleId = 1, GameId = 1, Time = 123 } } },
+                new UserEntity { Id = 2, UserName = "Test 2" },
+            });
+
+            _userRepository = new UserRepository(_dbContextMock.Context);
+        }
+
+        [Fact]
+        public async Task ShouldReturnListOfUsers()
+        {
+            var users = await _userRepository.GetAll();
+
+            Assert.NotNull(users);
+            Assert.True(users.First().Runs.Any());
+            Assert.Equal(2, users.Count);
+        }
+    }
+}
