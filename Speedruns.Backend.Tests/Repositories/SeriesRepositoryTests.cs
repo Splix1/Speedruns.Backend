927aa34fb6dae7aac362b6f048a774a9ae1ae45e
Splix1 <unversed1241@yahoo.com>
2023-10-25 18:04:59 -0700

    (FEAT): Repository for testing series, test for returning list of series

---
+++ Speedruns.Backend.Tests/Repositories/SeriesRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Speedruns.Backend.Entities;
+using Speedruns.Backend.Repositories;
+using Speedruns.Backend.Tests.Database;
+
+namespace Speedruns.Backend.Tests.Repositories
+{
+    public class SeriesRepositoryTests
+    {
+        private DbContextMock<SeriesEntity> _dbContextMock;
+        private SeriesRepository _seriesRepository;
+
+        public SeriesRepositoryTests()
+        {
+            _dbContextMock = new DbContextMock<SeriesEntity>(new List<SeriesEntity>
+            {
+                new SeriesEntity { Id = 1, Name = "Series 1", Players = 100 },
+                new SeriesEntity { Id = 2, Name = "Series 2", Players = 200 }
+            });
+
+            _seriesRepository = new SeriesRepository(_dbContextMock.Context);
+        }
+
+        [Fact]
+        public async Task ShouldReturnListOfSeries()
+        {
+            var series = await _seriesRepository.GetAll();
+
+            Assert.NotNull(series);
+            Assert.Equal(2, series.Count);
+            Assert.Equal("Series 1", series.First().Name);
+            Assert.Equal("Series 2", series.Last().Name);
+        }
+    }
+}
