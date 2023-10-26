5720d257aa9361a0ef9da2ccd8e5e17871be9d40
Splix1 <unversed1241@yahoo.com>
2023-10-25 17:37:13 -0700

    (FEAT): Repository for comments repository testing, along with returning all the comments for a specific run test

---
+++ Speedruns.Backend.Tests/Repositories/CommentsRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Speedruns.Backend.Entities;
+using Speedruns.Backend.Repositories;
+using Speedruns.Backend.Tests.Database;
+
+namespace Speedruns.Backend.Tests.Repositories
+{
+    public class CommentsRepositoryTests
+    {
+        private DbContextMock<CommentEntity> _dbContextMock;
+        private CommentsRepository _commentsRepository;
+
+        public CommentsRepositoryTests()
+        {
+            _dbContextMock = new DbContextMock<CommentEntity>(new List<CommentEntity>
+            {
+                new CommentEntity { Id = 1, Date = DateTime.UtcNow, Text = "Comment 1", UserId = 1, RunId = 1 },
+                new CommentEntity { Id = 2, Date = DateTime.UtcNow, Text = "Comment 2", UserId = 1, RunId = 1 },
+            });
+
+            _commentsRepository = new CommentsRepository(_dbContextMock.Context);
+        }
+
+        [Fact]
+        public async Task ShouldReturnCommentsByRunId()
+        {
+            var comments = await _commentsRepository.GetComments(1);
+
+            Assert.NotNull(comments);
+            Assert.IsType<List<CommentEntity>>(comments);
+            Assert.Equal(2, comments.Count);
+            Assert.Equal("Comment 1", comments.First().Text);
+            Assert.Equal("Comment 2", comments.Last().Text);
+        }
+    }
+}
