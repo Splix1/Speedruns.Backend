28aed6bb5df803d519b9afdd574931c30202918d
Splix1 <unversed1241@yahoo.com>
2023-10-28 14:37:12 -0700

    (FEAT): Comments controller test and return 200 get all test

---
+++ Speedruns.Backend.Tests/Controllers/CommentControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Speedruns.Backend.Controllers;
+using Speedruns.Backend.Entities;
+using Speedruns.Backend.Interfaces;
+using System.Net;
+
+namespace Speedruns.Backend.Tests.Controllers
+{
+    public class CommentControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturn200GetAll()
+        {
+            var commentsRepositoryMock = Substitute.For<ICommentsRepository>();
+
+            commentsRepositoryMock.GetComments(Arg.Any<long>()).Returns(new List<CommentEntity>
+            {
+                new CommentEntity { Id = 1, Text = "Test 1" },
+                new CommentEntity { Id = 2, Text = "Test 2" }
+            });
+
+            var controller = new CommentController(commentsRepositoryMock);
+
+            var response = await controller.GetAll(1);
+
+            var result = response.Result as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
