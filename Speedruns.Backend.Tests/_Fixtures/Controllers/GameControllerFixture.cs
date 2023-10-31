5d30168ffacb1a7f1a8c814ec4931132926764ea
Splix1 <unversed1241@yahoo.com>
2023-10-31 14:44:35 -0700

    (FEAT): Game controller tests use a fixture and reset substitutes upon tests

---
+++ Speedruns.Backend.Tests/_Fixtures/Controllers/GameControllerFixture.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using Speedruns.Backend.Interfaces;
+
+namespace Speedruns.Backend.Tests._Fixtures.Controllers
+{
+    public class GameControllerFixture
+    {
+        private IGamesRepository _gamesRepository;
+
+        public GameControllerFixture()
+        {
+            _gamesRepository = Substitute.For<IGamesRepository>();
+        }
+
+        internal IGamesRepository Repository => _gamesRepository;
+
+        public void ResetSubstitutes()
+        {
+            _gamesRepository = Substitute.For<IGamesRepository>();
+        }
+    }
+}
