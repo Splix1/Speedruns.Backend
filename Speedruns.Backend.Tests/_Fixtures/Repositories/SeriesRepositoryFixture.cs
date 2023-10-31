6ee3b5edb81525ac72a839bbbf987d7fc433bf6b
Splix1 <unversed1241@yahoo.com>
2023-10-31 14:04:57 -0700

    (FEAT): Series repository tests uses a fixture

---
+++ Speedruns.Backend.Tests/_Fixtures/Repositories/SeriesRepositoryFixture.cs
@@ -0,0 +1,25 @@
+﻿using Speedruns.Backend.Entities;
+using Speedruns.Backend.Repositories;
+using Speedruns.Backend.Tests.Database;
+
+namespace Speedruns.Backend.Tests._Fixtures.Repositories
+{
+    public class SeriesRepositoryFixture
+    {
+        private DbContextMock<SeriesEntity> _dbContextMock;
+        private SeriesRepository _seriesRepository;
+
+        public SeriesRepositoryFixture()
+        {
+            _dbContextMock = new DbContextMock<SeriesEntity>(new List<SeriesEntity>
+            {
+                new SeriesEntity { Id = 1, Name = "Series 1", Players = 100 },
+                new SeriesEntity { Id = 2, Name = "Series 2", Players = 200 }
+            });
+
+            _seriesRepository = new SeriesRepository(_dbContextMock.Context);
+        }
+
+        internal SeriesRepository Repository => _seriesRepository;
+    }
+}
